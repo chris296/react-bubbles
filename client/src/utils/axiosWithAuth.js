b8ac3e8166862cd92c16a9d5d7fb8b8111f4dad4
chris296 <chris.rios296@gmail.com>
2020-03-27 09:41:44 -0700

    first commit

---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    return axios.create({
+        baseUrl: 'http://localhost:5000',
+        headers: {
+            Authorization: token
+        }
+    })
+}
